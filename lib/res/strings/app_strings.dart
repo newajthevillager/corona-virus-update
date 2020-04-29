a9514d2b179c24ac8d9fdbc3da200e53ac1e353e
newaj <linuxunixstorm@gmail.com>
2020-04-29 10:42:08 +0600

    api version 2

--- lib/res/strings/app_strings.dart
+++ lib/res/strings/app_strings.dart
@@ -1,6 +1,6 @@
 class AppString {
 
-  static String corona_summary_url = "https://corona.lmao.ninja/all";
-  static String corona_countries_url = "https://corona.lmao.ninja/countries";
+  static String corona_summary_url = "https://corona.lmao.ninja/v2/all";
+  static String corona_countries_url = "https://corona.lmao.ninja/v2/countries";
   
 }
